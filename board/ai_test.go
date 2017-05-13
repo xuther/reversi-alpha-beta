e9f02363f0a9b7e52eef29b57f552f5b7e5be33b
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-05-13 14:15:22 -0600

    Timing tests

--- board/ai_test.go
+++ board/ai_test.go
@@ -3,6 +3,7 @@ package board
 import (
 	"log"
 	"testing"
+	"time"
 
 	"github.com/xuther/reversi-alpha-beta/ab"
 )
@@ -22,9 +23,18 @@ func TestAI(t *testing.T) {
 		maximizingFor: 1,
 	}
 
+	start := time.Now()
 	utility, path := ab.Search(Root, 3, -1000, 1000)
+	nonParallel := time.Since(start)
 
+	start = time.Now()
+	putility, ppath := ab.StartSearchMulti(Root, 3, -1000, 1000)
+	parallel := time.Since(start)
+
+	log.Printf("Non Parallel Time: %s", nonParallel)
 	log.Printf("Utility: %v, Path: %v", utility, path)
+	log.Printf("Parallel Time: %s", parallel)
+	log.Printf("Utility: %v, Path: %v", putility, ppath)
 }
 
 func TestAIB(t *testing.T) {
@@ -36,10 +46,18 @@ func TestAIB(t *testing.T) {
 		maximizingFor: 1,
 	}
 
+	start := time.Now()
 	utility, path := ab.Search(Root, 5, -1000, 1000)
+	nonParallel := time.Since(start)
+
+	start = time.Now()
+	putility, ppath := ab.StartSearchMulti(Root, 5, -1000, 1000)
+	parallel := time.Since(start)
 
 	log.Printf("Utility: %v, Path: %v", utility, path)
-	log.Printf("Final Board:")
+	log.Printf("Non Parallel Time: %s", nonParallel)
+	log.Printf("Parallel Time: %s", parallel)
+	log.Printf("Utility: %v, Path: %v", putility, ppath)
 
 }
 func TestAIC(t *testing.T) {
@@ -51,8 +69,17 @@ func TestAIC(t *testing.T) {
 		maximizingFor: 1,
 	}
 
+	start := time.Now()
 	utility, path := ab.Search(Root, 9, -1000, 1000)
+	nonParallel := time.Since(start)
+
+	start = time.Now()
+	putility, ppath := ab.StartSearchMulti(Root, 9, -1000, 1000)
+	parallel := time.Since(start)
+	time.Sleep(3 * time.Second)
 
 	log.Printf("Utility: %v, Path: %v", utility, path)
-	log.Printf("Final Board:")
+	log.Printf("Non Parallel Time: %s", nonParallel)
+	log.Printf("Parallel Time: %s", parallel)
+	log.Printf("Utility: %v, Path: %v", putility, ppath)
 }
